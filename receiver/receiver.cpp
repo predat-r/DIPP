5d623d659c67b7443a5750ec71146a31b146ddd4
predat-r <harisnaeem612@outlook.com>
2025-04-27 00:05:39 +0500

    implementation of rabbitmq

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -1,10 +1,15 @@
+#include "RabbitMqConnectionHandler.h"
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <amqpcpp.h>
+
 using namespace std;
 using namespace cv; 
+using namespace  AMQP;
 
 int main()
-{
+{  
+    RabbitMqConnectionHandler Handler(1);
     Mat image = imread("../image.jpg");         
     
     if (image.empty()) {
