8b87efb09ae7ae9c05897720d7c5b239f544a8ae
predat-r <harisnaeem612@outlook.com>
2025-04-27 01:00:50 +0500

    milestone: successful linking of libraries, path issues solved, and connections established

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -1,20 +1,61 @@
-#include "RabbitMqConnectionHandler.h"
+#include <RabbitMqConnectionHandler.h>
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <amqpcpp.h>
+#include <unistd.h> // For close() function
 
 using namespace std;
-using namespace cv; 
-using namespace  AMQP;
+using namespace cv;
+using namespace AMQP;
 
 int main()
-{  
-    RabbitMqConnectionHandler Handler(1);
-    Mat image = imread("../image.jpg");         
-    
-    if (image.empty()) {
-        cout << "Failed to load image!";
+{
+    try
+    {
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+        if (sockfd < 0)
+        {
+            std::cerr << "Failed to create socket" << std::endl;
+            return -1;
+        }
+
+        RabbitMqConnectionHandler handlerInstance(sockfd);
+
+        AMQP::Connection* connection = handlerInstance.establishConnection("localhost", 5672, "guest", "guest", "/", sockfd);
+
+        Channel channel(connection);
+
+        channel.declareQueue("images");
+
+        Mat image = imread("../image.jpg");
+        if (image.empty())
+        {
+            cerr << "Failed to load image!" << endl;
+            delete connection;
+            close(sockfd);
+            return -1;
+        }
+        cout << "Image loaded" << endl;
+
+        // Create a handler instance to access non-static methods
+        RabbitMqConnectionHandler handler(sockfd);
+
+        // Send the image
+        handler.sendImage(channel, "images", image, "jpg");
+        cout << "Image sent" << endl;
+
+        // Wait a moment to ensure message is sent before closing
+        sleep(1);
+
+        // Clean up
+        delete connection;
+        close(sockfd);
+
+        return 0;
+    }
+    catch (const exception &e)
+    {
+        cerr << "Error: " << e.what() << endl;
         return -1;
-    }      
-    cout <<"image loaded";
+    }
 }
